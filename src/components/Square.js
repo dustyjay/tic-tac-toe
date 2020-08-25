da084d2993b2f63e604d0d54e2b02a7a158b70d4
janearisah <janet@chaka.ng>
2020-08-25 20:08:43 +0100

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
+}
+
+Square.propTypes = {
+  value: PropTypes.string
+};
+
+export default Square;
